95409431b479fcc4b8d6aca4d6afb7992cc75863
itaycohen <itay.c14@gmail.com>
2018-10-24 12:04:43 +0300

    5

--- app/src/main/java/com/interview/itaycohen/assignmentyit/BrowsePhotosActivity.kt
+++ app/src/main/java/com/interview/itaycohen/assignmentyit/BrowsePhotosActivity.kt
@@ -40,6 +40,7 @@ class BrowsePhotosActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //
         binding = DataBindingUtil.setContentView(this, R.layout.activity_browse_photos)
         setSupportActionBar(findViewById(R.id.toolbar))
 
